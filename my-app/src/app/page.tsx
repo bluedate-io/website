959f33e13722a98995d6a1d7dec9ba6b02bfabcd
saitadikonda99 <saitadikonda.tech@gmail.com>
2026-03-20 11:59:36 +0530

    Refactor code structure for improved readability and maintainability

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,22 +1,10 @@
-import Navbar from "./components/navbar/navbar";
-import Footer from "./components/footer/footer";
-import Hero from "./ui/hero/hero";
-import Problem from "./ui/problem/problem";
-import Match from "./ui/match/match";
-import Faq from "./ui/faq/faq";
-import NotificationBanner from "./components/notificationbanner/notificationbanner";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="HomeComponent">
       <div className="HomeComponent__in">
-        <Navbar />
-        <Hero />
-        <Problem />
-        <Match />
-        <Faq />
-        <Footer />
-        <NotificationBanner />
+        
       </div>
     </div>
   );
